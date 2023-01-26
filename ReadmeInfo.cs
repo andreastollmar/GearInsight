1843a4ea70ebc07c08fd4135a6208ec65977124b
jesperkeden <83838381+jesperkeden@users.noreply.github.com>
2023-01-26 12:41:51 +0100

    update to readmeinfo

---
+++ ReadmeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testWoW
+{
+    internal class ReadmeInfo
+    {
+        //BILD
+        //För att få ut en bild från RequestResult<ItemMedia> media = await warcraftClient.GetItemMediaAsync(200337, "static-eu");
+        //Så måste vi välja Assets > d.Value.AbsoluteUri
+    }
+}
