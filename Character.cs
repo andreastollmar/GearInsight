24fd6fcbf416c61801c099cc05710f794471d32e
jesperkeden <83838381+jesperkeden@users.noreply.github.com>
2023-01-27 15:49:57 +0100

    test and creation of Character class

---
+++ Character.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testWoW.Interface;
+
+namespace testWoW
+{
+    public class Character
+    {
+        public Head Head { get; set; } = new Head();
+        public Neck Neck { get; set; } = new Neck();
+        public Shoulder Shoulder { get; set; } = new Shoulder();
+        public Chest Chest { get; set; } = new Chest();
+        public Waist Waist { get; set; } = new Waist();
+        public Legs Legs { get; set; } = new Legs();
+        public Feet Feet { get; set; } = new Feet();
+        public Wrist Wrist { get; set; } = new Wrist();
+        public Gloves Gloves { get; set; } = new Gloves();
+        public Ring1 Ring1 { get; set; } = new Ring1();
+        public Ring2 Ring2 { get; set; } = new Ring2();
+        public Trinket1 Trinket1 { get; set; } = new Trinket1();
+        public Trinket2 Trinket2 { get; set; } = new Trinket2();
+        public Cloak Cloak { get; set; } = new Cloak();
+        public Mainhand Mainhand { get; set; } = new Mainhand();
+        public Offhand Offhand { get; set; } = new Offhand();
+        public Tabard Tabard { get; set; } = new Tabard();
+        public List<IEquipment> equipment { get; set; }
+
+        //public Character()
+        //{
+        //    equipment = Item.CreateItems();
+        //}
+    }
+}
--- Interface/IEquipment.cs
+++ Interface/IEquipment.cs
@@ -6,7 +6,155 @@ using System.Threading.Tasks;
 
 namespace testWoW.Interface
 {
-    internal interface IEquipment
+    public interface IEquipment
     {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+    }
+
+    public class Item
+    {
+        public static List<IEquipment> CreateItems()
+        {
+            List<IEquipment> equipment = new List<IEquipment>();
+            equipment.Add(new Head());
+            equipment.Add(new Neck());
+            equipment.Add(new Shoulder());
+            equipment.Add(new Chest());
+            equipment.Add(new Waist());
+            equipment.Add(new Legs());
+            equipment.Add(new Feet());
+            equipment.Add(new Wrist());
+            equipment.Add(new Gloves());
+            equipment.Add(new Ring1());
+            equipment.Add(new Ring2());
+            equipment.Add(new Trinket1());
+            equipment.Add(new Trinket2());
+            equipment.Add(new Cloak());
+            equipment.Add(new Mainhand());
+            equipment.Add(new Offhand());
+            equipment.Add(new Tabard());
+            return equipment;
+        }
+    }
+
+    public class Head : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Neck : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Shoulder : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Chest : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Waist : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Legs : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Feet : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Wrist : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Gloves : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Ring1 : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Ring2 : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Trinket1 : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Trinket2 : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Cloak : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Mainhand : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Offhand : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
+    }
+    public class Tabard : Item, IEquipment
+    {
+        public string Icon { get; set; }
+        public int wowheadId { get; set; }
+        // hover = någon href som ger komplett item
+        // hyperlink till wowhead
     }
 }
